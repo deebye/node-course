d86251c627aae3d6e795d77daeaecdcd526a77a3
Derek Bye <dbye@fct.ca>
2023-12-07 13:53:24 -0500

    Add middleware for '/add/:id' route and update default route

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require('express')
 const app = express()
 
+app.use('/add/:id', (req, res) => {
+    console.log('First middleware', req.params)
+    res.send('<h1>Add!</h1>')
+})
+
+app.use('/', (req, res) => {
+    console.log('Second middleware')
+    res.send('<h1>Hello from Express!</h1>')
+})
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
 })
